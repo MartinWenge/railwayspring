effeb2b3358fa221b1a601323b7a564e3e6b5e84
Martin Wengenmayr <Martin.Wengenmayr@pta.de>
2024-03-20 12:45:58 +0100

    add simple angular ngrx app

---
+++ frontend/railwayspringapp/src/app/app.config.ts
@@ -0,0 +1,22 @@
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+
+import { counterReducer } from './counter.reducer';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(routes),
+    provideStore(
+      {
+        count: counterReducer
+      }
+    ),
+    provideEffects(),
+    provideStoreDevtools({ maxAge: 25})
+  ]
+};
---
+++ frontend/railwayspringapp/src/app/my-counter/my-counter.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
+import { increment, decrement, reset } from '../counter.actions';
+import { Store } from '@ngrx/store';
+
+@Component({
+  selector: 'app-my-counter',
+  standalone: true,
+  imports: [AsyncPipe],
+  templateUrl: './my-counter.component.html',
+  styleUrl: './my-counter.component.css'
+})
+
+export class MyCounterComponent {
+  count$: Observable<number>;
+
+  constructor(private store: Store<{ count: number }>) {
+    this.count$ = store.select('count');
+  }
+
+  increment() {
+    this.store.dispatch(increment());
+  }
+
+  decrement() {
+    this.store.dispatch(decrement());
+  }
+
+  reset() {
+    this.store.dispatch(reset());
+  }
+}
