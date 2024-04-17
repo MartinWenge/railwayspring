aaa0cc0c5788bfb8a2d76601f1560ef89c128df5
Martin Wengenmayr <Martin.Wengenmayr@pta.de>
2024-04-17 10:57:41 +0200

    add cors config to java backend

--- backend/src/main/java/com/wengenm/railwayspring/controller/AddOneController.java
+++ backend/src/main/java/com/wengenm/railwayspring/controller/AddOneController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class AddOneController {
 
     @PostMapping(value = "/addone")
-    //@CrossOrigin(origins = "http://localhost:4200")
     public ResponseEntity<SimpleNumber> addOne(@RequestBody SimpleNumber counter){
         SimpleNumber counterPlusOne = new SimpleNumber(counter.getCount() + 1);
         return new ResponseEntity<>(counterPlusOne, HttpStatus.OK);
